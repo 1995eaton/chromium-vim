e0ad8a0f9bd36b10a8197334ceb0dd86a04b6507
1995eaton <1995eaton@gmail.com>
2014-06-06 14:34:35 -0400

    Attempt at fixing #48

--- content_scripts/cursor.js
+++ content_scripts/cursor.js
@@ -2,16 +2,7 @@ var Cursor = {}; // Hide the mouse cursor on keydown (Linux)
 
 Cursor.init = function() {
   this.overlay = document.createElement("div");
-  this.overlay.style.position = "fixed";
-  this.overlay.style.left = "0";
-  this.overlay.style.width = "100%";
-  this.overlay.style.top = "0";
-  this.overlay.style.height = "100%";
-  this.overlay.style.zIndex = "999999999";
-  this.overlay.style.cursor = "none";
-  this.overlay.style.display = "none";
-  this.overlay.style.webkitTransform = "translateZ(0)";
-  this.overlay.style.transform = "translateZ(0)";
+  this.overlay.id = "cVim-cursor";
   document.body.appendChild(this.overlay);
   var oldX, oldY;
   document.addEventListener("mousemove", function(e) {
