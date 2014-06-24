417b92408f6667d7af246866a4dddd4302c28a20
itchyny <neknekneknek.t@gmail.com>
2014-06-25 08:51:58 +0900

    follow the coding style

--- content_scripts/clipboard.js
+++ content_scripts/clipboard.js
@@ -1,15 +1,15 @@
-var Clipboard = {
-  store: "",
-  copy: function(text, store) {
-    if (!store) {
-      this.store = text;
-    } else {
-      this.store += (this.store.length ? "\n" : "") + text;
-    }
-    chrome.runtime.sendMessage({action: "copy", text: this.store});
-  },
-  paste: function(tabbed) {
-    return chrome.runtime.sendMessage({action: (tabbed ? "openPasteTab" : "openPaste")});
-  }
+var Clipboard = {};
+Clipboard.store = "";
 
+Clipboard.copy = function(text, store) {
+  if (!store) {
+    this.store = text;
+  } else {
+    this.store += (this.store.length ? "\n" : "") + text;
+  }
+  chrome.runtime.sendMessage({action: "copy", text: this.store});
+};
+
+Clipboard.paste = function(tabbed) {
+  return chrome.runtime.sendMessage({action: (tabbed ? "openPasteTab" : "openPaste")});
 };
