84a93f1efa0b8f668d40d5eee036abab2c87abdc
1995eaton <1995eaton@gmail.com>
2014-09-12 13:34:35 -0400

    more scroll improvements

--- compiled.js
+++ compiled.js
@@ -4744,30 +4744,199 @@ if (!Command.loaded) {
 var Scroll = {};
 Scroll.positions = {};
 
-
 var ease = {
-  outSine: function(t, b, c, d) {
-    return c * Math.sin(t/d * (Math.PI/2)) + b;
-  },
-  outQuint: function(t, b, c, d) {
-    t /= d;
-    t--;
-    return c*(t*t*t*t*t + 1) + b;
+  // jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
+  // Open source under the BSD License.
+  // Copyright © 2008 George McGinley Smith
+  // All rights reserved.
+  // https://raw.github.com/danro/jquery-easing/master/LICENSE
+  inQuad: function(t, b, c, d) {
+    return c*(t/=d)*t + b;
   },
   outQuad: function(t, b, c, d) {
-    t /= d;
-    return -c * t*(t-2) + b;
+    return -c *(t/=d)*(t-2) + b;
   },
-  outExpo: function(t, b, c, d) {
-    return c * ( -Math.pow( 2, -10 * t/d ) + 1 ) + b;
+  inOutQuad: function(t, b, c, d) {
+    if ((t /= d/2) < 1) {
+      return c/2*t*t + b;
+    }
+    return -c/2 * ((--t)*(t-2) - 1) + b;
+  },
+  inCubic: function(t, b, c, d) {
+    return c*(t/=d)*t*t + b;
+  },
+  outCubic: function(t, b, c, d) {
+    return c*((t=t/d-1)*t*t + 1) + b;
+  },
+  inOutCubic: function(t, b, c, d) {
+    if ((t /= d/2) < 1) {
+      return c/2*t*t*t + b;
+    }
+    return c/2*((t-=2)*t*t + 2) + b;
+  },
+  inQuart: function(t, b, c, d) {
+    return c*(t/=d)*t*t*t + b;
   },
   outQuart: function(t, b, c, d) {
     return -c * ((t=t/d-1)*t*t*t - 1) + b;
   },
+  inOutQuart: function(t, b, c, d) {
+    if ((t /= d/2) < 1) {
+      return c/2*t*t*t*t + b;
+    }
+    return -c/2 * ((t-=2)*t*t*t - 2) + b;
+  },
+  inQuint: function(t, b, c, d) {
+    return c*(t/=d)*t*t*t*t + b;
+  },
+  outQuint: function(t, b, c, d) {
+    return c*((t=t/d-1)*t*t*t*t + 1) + b;
+  },
+  inOutQuint: function(t, b, c, d) {
+    if ((t /= d/2) < 1) {
+      return c/2*t*t*t*t*t + b;
+    }
+    return c/2*((t-=2)*t*t*t*t + 2) + b;
+  },
+  inSine: function(t, b, c, d) {
+    return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+  },
+  outSine: function(t, b, c, d) {
+    return c * Math.sin(t/d * (Math.PI/2)) + b;
+  },
+  inOutSine: function(t, b, c, d) {
+    return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+  },
+  inExpo: function(t, b, c, d) {
+    return (t===0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+  },
+  outExpo: function(t, b, c, d) {
+    return (t===d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+  },
+  inOutExpo: function(t, b, c, d) {
+    if (t===0) {
+      return b;
+    }
+    if (t===d) {
+      return b+c;
+    }
+    if ((t /= d/2) < 1) {
+      return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+    }
+    return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+  },
+  inCirc: function(t, b, c, d) {
+    return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+  },
   outCirc: function(t, b, c, d) {
-    t /= d;
-    t--;
-    return c * Math.sqrt(1 - t*t) + b;
+    return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+  },
+  inOutCirc: function(t, b, c, d) {
+    if ((t /= d/2) < 1) {
+      return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+    }
+    return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+  },
+  inElastic: function(t, b, c, d) {
+    var s=1.70158;var p=0;var a=c;
+    if (t===0) {
+      return b;
+    }
+    if ((t/=d)===1) {
+      return b+c;
+    }
+    if (!p) {
+      p=d*0.3;
+    }
+    if (a < Math.abs(c)) {
+      a=c;
+      s=p/4;
+    } else {
+      s = p/(2*Math.PI) * Math.asin (c/a);
+    }
+    return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+  },
+  outElastic: function(t, b, c, d) {
+    var s=1.70158;var p=0;var a=c;
+    if (t===0) {
+      return b;
+    }
+    if ((t/=d)===1) {
+      return b+c;
+    }
+    if (!p) {
+      p=d*0.3;
+    }
+    if (a < Math.abs(c)) {
+      a=c;
+      s=p/4;
+    } else {
+      s = p/(2*Math.PI) * Math.asin (c/a);
+    }
+    return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+  },
+  inOutElastic: function(t, b, c, d) {
+    var s=1.70158;var p=0;var a=c;
+    if (t===0) {
+      return b;
+    }
+    if ((t /= d/2)===2) {
+      return b+c;
+    }
+    if (!p) {
+      p=d*(0.3*1.5);
+    }
+    if (a < Math.abs(c)) {
+      a=c;
+      s=p/4;
+    } else {
+      s = p/(2*Math.PI) * Math.asin (c/a);
+    }
+    if (t < 1) {
+      return -0.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+    }
+    return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*0.5 + c + b;
+  },
+  inBack: function(t, b, c, d, s) {
+    if (s === undefined) {
+      s = 1.70158;
+    }
+    return c*(t/=d)*t*((s+1)*t - s) + b;
+  },
+  outBack: function(t, b, c, d, s) {
+    if (s === undefined) {
+      s = 1.70158;
+    }
+    return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+  },
+  inOutBack: function(t, b, c, d, s) {
+    if (s === undefined) {
+      s = 1.70158;
+    }
+    if ((t /= d/2) < 1) {
+      return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+    }
+    return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+  },
+  outBounce: function(t, b, c, d) {
+    if ((t/=d) < (1/2.75)) {
+      return c*(7.5625*t*t) + b;
+    } else if (t < (2/2.75)) {
+      return c*(7.5625*(t-=(1.5/2.75))*t + 0.75) + b;
+    } else if (t < (2.5/2.75)) {
+      return c*(7.5625*(t-=(2.25/2.75))*t + 0.9375) + b;
+    } else {
+      return c*(7.5625*(t-=(2.625/2.75))*t + 0.984375) + b;
+    }
+  },
+  inBounce: function(t, b, c, d) {
+    return c - ease.outBounce (d-t, 0, c, d) + b;
+  },
+  inOutBounce: function(t, b, c, d) {
+    if (t < d/2) {
+      return ease.inBounce (t*2, 0, c, d) * 0.5 + b;
+    }
+    return ease.outBounce (t*2-d, 0, c, d) * 0.5 + c*0.5 + b;
   }
 };
 
@@ -4777,24 +4946,35 @@ var ease = {
   var scroll = {
     x0: 0,
     y0: 0,
+    xc: 0,
     x1: 0,
     y1: 0,
+    yc: 0,
     tx: 0,
-    ty: 0
+    txo: 0,
+    tyo: 0,
+    ty: 0,
+    dx: 0,
+    dy: 0
   };
   var easeFn = ease.outExpo;
   var scrollYFunction = function() {
-    var delta = easeFn(scroll.ty++,
+    var delta = easeFn(scroll.ty,
         scroll.y0,
         scroll.y1 - scroll.y0,
-        settings.scrollduration);
-    scroll.y0 = delta;
-    window.scrollTo(window.scrollX, scroll.y0);
-    if (scroll.ty < settings.scrollduration && Math.abs(scroll.y1 - scroll.y0) > 1) {
+        scroll.dy);
+    var time = Date.now();
+    scroll.yc = delta;
+    scroll.ty += time - scroll.tyo;
+    scroll.tyo = time;
+    window.scrollTo(window.scrollX, delta);
+    if (scroll.ty < scroll.dy) {
       animationYFrame = window.requestAnimationFrame(scrollYFunction);
     } else {
       window.cancelAnimationFrame(animationYFrame);
+      window.scrollTo(window.scrollX, scroll.y1);
       scroll.y0 = 0;
+      scroll.yc = 0;
       scroll.y1 = 0;
       scroll.ty = 0;
     }
@@ -4803,49 +4983,65 @@ var ease = {
     var delta = easeFn(scroll.tx++,
         scroll.x0,
         scroll.x1 - scroll.x0,
-        settings.scrollduration);
-    scroll.x0 = delta;
-    window.scrollTo(scroll.x0, window.scrollY);
-    if (scroll.tx < settings.scrollduration && Math.abs(scroll.x1 - scroll.x0) > 1) {
+        scroll.dx);
+    var time = Date.now();
+    scroll.xc = delta;
+    scroll.tx += time - scroll.txo;
+    scroll.txo = time;
+    window.scrollTo(delta, window.scrollY);
+    if (scroll.tx < scroll.dx) {
       animationXFrame = window.requestAnimationFrame(scrollXFunction);
     } else {
       window.cancelAnimationFrame(animationXFrame);
+      window.scrollTo(scroll.x1, window.scrollY);
       scroll.x0 = 0;
       scroll.x1 = 0;
+      scroll.xc = 0;
       scroll.tx = 0;
     }
   };
-  Scroll.smoothScrollTo = function(x, y) {
+  window.setSmoothScrollEaseFN = function(fn) {
+    easeFn = fn;
+  };
+  window.smoothScrollTo = function(x, y, d) {
+    window.cancelAnimationFrame(animationXFrame);
+    window.cancelAnimationFrame(animationYFrame);
+    scroll.dx = d;
+    scroll.dy = d;
     if (x !== window.scrollX) {
-      window.cancelAnimationFrame(animationXFrame);
       scroll.x0 = window.scrollX;
       scroll.x1 = x;
       scroll.tx = 0;
+      scroll.txo = Date.now();
       scrollXFunction();
     }
     if (y !== window.scrollY) {
-      window.cancelAnimationFrame(animationYFrame);
       scroll.y0 = window.scrollY;
       scroll.y1 = y;
       scroll.ty = 0;
+      scroll.tyo = Date.now();
       scrollYFunction();
     }
   };
-  Scroll.smoothScrollBy = function(x, y) {
-    var oldDy = scroll.y1 - scroll.y0;
-    var oldDx = scroll.x1 - scroll.x0;
+  window.smoothScrollBy = function(x, y, d) {
+    var oldDy = scroll.y1 - scroll.yc;
+    var oldDx = scroll.x1 - scroll.xc;
     if (x) {
       window.cancelAnimationFrame(animationXFrame);
+      scroll.dx = d;
       scroll.x0 = window.scrollX;
       scroll.x1 = oldDx + scroll.x0 + x;
       scroll.tx = 0;
+      scroll.txo = Date.now();
       scrollXFunction();
     }
     if (y) {
       window.cancelAnimationFrame(animationYFrame);
+      scroll.dy = d;
       scroll.y0 = window.scrollY;
       scroll.y1 = oldDy + scroll.y0 + y;
       scroll.ty = 0;
+      scroll.tyo = Date.now();
       scrollYFunction();
     }
   };
@@ -4863,40 +5059,40 @@ Scroll.scroll = function(type, repeats) {
 
     switch (type) {
       case 'down':
-        Scroll.smoothScrollBy(0, repeats * stepSize);
+        window.smoothScrollBy(0, repeats * stepSize, settings.scrollduration);
         break;
       case 'up':
-        Scroll.smoothScrollBy(0, -repeats * stepSize);
+        window.smoothScrollBy(0, -repeats * stepSize, settings.scrollduration);
         break;
       case 'pageDown':
-        Scroll.smoothScrollBy(0, repeats * window.innerHeight / 2);
+        window.smoothScrollBy(0, repeats * window.innerHeight / 2, settings.scrollduration);
         break;
       case 'fullPageDown':
-        Scroll.smoothScrollBy(0, repeats * window.innerHeight * (settings.fullpagescrollpercent / 100 || 0.85));
+        window.smoothScrollBy(0, repeats * window.innerHeight * (settings.fullpagescrollpercent / 100 || 1), settings.scrollduration);
         break;
       case 'pageUp':
-        Scroll.smoothScrollBy(0, -repeats * window.innerHeight / 2);
+        window.smoothScrollBy(0, -repeats * window.innerHeight / 2, settings.scrollduration);
         break;
       case 'fullPageUp':
-        Scroll.smoothScrollBy(0, -repeats * window.innerHeight * (settings.fullpagescrollpercent / 100 || 0.85));
+        window.smoothScrollBy(0, -repeats * window.innerHeight * (settings.fullpagescrollpercent / 100 || 1), settings.scrollduration);
         break;
       case 'top':
-        Scroll.smoothScrollBy(0, -document.body.scrollTop);
+        window.smoothScrollBy(0, -document.body.scrollTop, settings.scrollduration);
         break;
       case 'bottom':
-        Scroll.smoothScrollBy(0, document.body.scrollHeight - document.body.scrollTop - window.innerHeight + 20);
+        window.smoothScrollTo(window.scrollX, document.body.scrollHeight - document.documentElement.clientHeight, settings.scrollduration);
         break;
       case 'left':
-        Scroll.smoothScrollBy(repeats * -stepSize / 2, 0);
+        window.smoothScrollBy(repeats * -stepSize / 2, 0, settings.scrollduration);
         break;
       case 'right':
-        Scroll.smoothScrollBy(repeats * stepSize / 2, 0);
+        window.smoothScrollBy(repeats * stepSize / 2, 0, settings.scrollduration);
         break;
       case 'leftmost':
-        Scroll.smoothScrollBy(-document.body.scrollLeft - 10, 0);
+        window.smoothScrollBy(-document.body.scrollLeft - 10, 0, settings.scrollduration);
         break;
       case 'rightmost':
-        Scroll.smoothScrollBy(document.body.scrollWidth - document.body.scrollLeft - window.innerWidth + 20, 0);
+        window.smoothScrollBy(document.body.scrollWidth - document.body.scrollLeft - window.innerWidth + 20, 0, settings.scrollduration);
         break;
       default:
         break;
