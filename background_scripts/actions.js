397c2de33873cead718754fce905aa6bbbd23201
1995eaton <1995eaton@gmail.com>
2014-10-18 14:45:27 -0400

    custom fix for #116

--- background_scripts/actions.js
+++ background_scripts/actions.js
@@ -317,17 +317,19 @@ Actions.goToTab = function() {
   });
 };
 
-Actions.moveTabRight = function() {
-  chrome.tabs.move(sender.tab.id, {
-    index: sender.tab.index + request.repeats
-  });
-};
-
-Actions.moveTabLeft = function() {
-  chrome.tabs.move(sender.tab.id, {
-    index: (sender.tab.index - request.repeats <= -1) ? 0 : sender.tab.index - request.repeats
-  });
-};
+(function() {
+  var move = function(by) {
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+      var ptabs = tabs.filter(function(e) { return e.pinned; });
+      chrome.tabs.move(sender.tab.id, {
+        index: Math.min( sender.tab.pinned ? ptabs.length - 1 : ptabs.length + tabs.length,
+                         Math.max(sender.tab.pinned ? 0 : ptabs.length, sender.tab.index + by) )
+      });
+    });
+  };
+  Actions.moveTabRight = function() { move( request.repeats); };
+  Actions.moveTabLeft  = function() { move(-request.repeats); };
+})();
 
 Actions.openPasteTab = function() {
   var paste = Clipboard.paste();
