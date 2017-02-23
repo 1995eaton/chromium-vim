0e249e0b1b96f43ee06803bc9c23e01114374990
Jake Eaton <Jake@Jakes-iMac.local>
2017-02-23 13:48:04 -0500

    make buffers in current window have priority

--- background_scripts/actions.js
+++ background_scripts/actions.js
@@ -697,15 +697,26 @@ Actions = (function() {
 
   _.getBuffers = function(o) {
     chrome.tabs.query({}, function(tabs) {
+      var otherWindows = [];
+      tabs = tabs.filter(function(e) {
+        if (e.windowId === o.sender.tab.windowId)
+          return true;
+        otherWindows.push(e);
+        return false;
+      });
+      tabs = tabs.concat(otherWindows);
+
+      var buffers = tabs.map(function(e, i) {
+        var title = e.title;
+        if (settings.showtabindices) {
+          title = title.replace(new RegExp('^' + (e.index + 1) + ' '), '');
+        }
+        return [(i + 1) + ': ' + title, e.url];
+      });
+
       o.callback({
         type: 'buffers',
-        buffers: tabs.map(function(e, i) {
-          var title = e.title;
-          if (settings.showtabindices) {
-            title = title.replace(new RegExp('^' + (e.index + 1) + ' '), '');
-          }
-          return [(i + 1) + ': ' + title, e.url, e.id];
-        })
+        buffers: buffers
       });
     });
   };
