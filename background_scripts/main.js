f3f05ce294f023c39e5ef1d03ec8312723f324e1
Jake Eaton <Jake@Jakes-iMac.local>
2017-02-27 18:36:37 -0500

    revert pr #502, this would not be a solution to #390

--- background_scripts/main.js
+++ background_scripts/main.js
@@ -161,10 +161,8 @@ var Listeners = {
         chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
           chrome.tabs.sendMessage(tab[0].id, {
             action: 'nextCompletionResult'
-          }, function(res) {
-            if(res == true){
-              chrome.windows.create({url: 'chrome://newtab'});
-            }
+          }, function() {
+            chrome.windows.create({url: 'chrome://newtab'});
           });
         });
         break;
